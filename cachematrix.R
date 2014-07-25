337220656c440e17396663f0ce85c21bb52c8649
Kurt Kessler <krkessler@gmail.com>
2014-07-25 12:01:10 -0400

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The makeCacheMatrix() will create a matrix from a parameter
+## and store that matrix in a global variable
+makeCacheMatrix <- function(x = as.matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(matrix) m <<- matrix
+    getmatrix <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
-
-
-## Write a short comment describing this function
-
+## The cacheSolve() will take the inverse of a matrix.  Before it takes the 
+## inverse, it will see if the inverse is stored in a global function.  If so,
+## it will use that gobal function and not go through the calculation of 
+## creating the inverse.  This saves computing time.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
 }
+
